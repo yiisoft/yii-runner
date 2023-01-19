dae4ba0a8905d92e44880d9478f668d9d8a8717a
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-01-19 16:38:43 +0300

    Add ability to configure config params group (#38)

--- src/ConfigFactory.php
+++ src/ConfigFactory.php
@@ -18,8 +18,13 @@ final class ConfigFactory
     /**
      * @throws ErrorException If the environment does not exist.
      */
-    public static function create(ConfigPaths $paths, ?string $environment): Config
+    public static function create(ConfigPaths $paths, ?string $environment, string $paramsGroup = 'params'): Config
     {
+        $paramsGroups = ['params'];
+        if ($paramsGroup !== 'params') {
+            $paramsGroups[] = $paramsGroup;
+        }
+
         $eventGroups = [
             'events',
             'events-web',
@@ -31,8 +36,9 @@ final class ConfigFactory
             $environment,
             [
                 ReverseMerge::groups(...$eventGroups),
-                RecursiveMerge::groups('params', ...$eventGroups),
+                RecursiveMerge::groups(...$paramsGroups, ...$eventGroups),
             ],
+            $paramsGroup,
         );
     }
 }
